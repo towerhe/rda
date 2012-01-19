3c73c40e120601406f5034bb1c4c99f53c9de71e
Tower He <towerhe@gmail.com>
2012-01-19 16:42:30 +0800

    Provided a method to config rda

---
+++ spec/lib/rda_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Rda do
+  describe '.config' do
+    subject { Rda.config }
+
+    it { should be_a(Confstruct::Configuration) }
+    its(:nginx_conf_paths) { should have(3).items }
+  end
+
+  describe '.configure' do
+    it 'sets the config paths of nginx' do
+      Rda.configure do
+        nginx_conf_paths ['/tmp/rda/nginx/conf']
+      end
+
+      Rda.config.nginx_conf_paths.should have(1).item
+      Rda.config.nginx_conf_paths.should include('/tmp/rda/nginx/conf')
+    end
+  end
+end
