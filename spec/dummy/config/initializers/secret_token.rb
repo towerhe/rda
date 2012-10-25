3e6d34f8562b6d20db637180e577b48a91359f88
Tower He <towerhe@gmail.com>
2012-10-26 00:50:48 +0800

    Removed the dummy rails app

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'c61dae1e483bcfeb8f070503d0237d18c9127fc08bf8df1ea3da570c30ac2393674f49cee9f699beb5c2f7671190b5fdab7c633328b3d666278e0911b89841e1'
