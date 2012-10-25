31af0fecb4e506f150b174cf86e2ff3917c2e98f
Tower He <towerhe@gmail.com>
2012-10-26 00:55:26 +0800

    Changed the current path for tests

--- spec/lib/rda/app_spec.rb
+++ spec/lib/rda/app_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe Rda::App do
   subject { Rda::App.new }
 
+  before(:all) { Dir.chdir(File.join(File.dirname(__FILE__), '../../dummy')) }
+
   describe '#restart' do
     it 'touches restart.txt' do
-      FileUtils.should_receive(:touch).with(::Rails.root.to_s + "/tmp/restart.txt")
+      FileUtils.should_receive(:touch).with(Rda::Rails.root.to_s + "/tmp/restart.txt")
 
       subject.restart
     end
