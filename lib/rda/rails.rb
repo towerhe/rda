9fafbeef7ed336304e1342a452c687b6b2b05932
Tower He <towerhe@gmail.com>
2012-11-08 12:03:27 +0800

    Check whether it is a valid rails app

--- lib/rda/rails.rb
+++ lib/rda/rails.rb
@@ -1,11 +1,19 @@
 module Rda
   module Rails
     def self.app_name
-      @app_name ||= IO.foreach(File.join(Dir.pwd, 'config.ru')) do |l|
-                      if l =~ /run\s+(.*)::Application/
-                        return $1.underscore.dasherize
+      @app_name ||= begin
+                      IO.foreach(File.join(Dir.pwd, 'config.ru')) do |l|
+                        if l =~ /run\s+(.*)::Application/
+                          return $1.underscore.dasherize
+                        end
                       end
+                    rescue
                     end
+
+      unless @app_name
+        $stderr.puts "ERROR: You should run rda under rails applications"
+        exit
+      end
     end
 
     def self.root
