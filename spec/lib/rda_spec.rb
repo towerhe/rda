7b190f3c4bdf43725f72a901427a844b364feb29
Tower He <towerhe@gmail.com>
2012-11-10 13:19:38 +0800

    Specs of loading config

--- spec/lib/rda_spec.rb
+++ spec/lib/rda_spec.rb
@@ -2,19 +2,30 @@ require 'spec_helper'
 
 describe Rda do
   describe '.config' do
+    let(:dummy_path) { File.join(File.dirname(__FILE__), '../dummy') }
+
+    before do
+      Rda::Rails.should_receive(:root).any_number_of_times.and_return(dummy_path)
+    end
+
     subject { Rda.config }
 
     it { should be_a(Confstruct::Configuration) }
+
+    its(:domain) { should == 'dummy.local' }
+    its(:rails_env) { should == 'development' }
+    specify { subject.nginx.conf_dir.should == '/opt/nginx/conf' }
+    specify { subject.passenger.user.should == 'root' }
+    specify { subject.passenger.group.should == 'root' }
   end
 
   describe '.configure' do
     it 'sets the config paths of nginx' do
       Rda.configure do
-        nginx_conf_paths ['/tmp/rda/nginx/conf']
+        rails_env 'production'
       end
 
-      Rda.config.nginx_conf_paths.should have(1).item
-      Rda.config.nginx_conf_paths.should include('/tmp/rda/nginx/conf')
+      Rda.config.rails_env.should == 'production'
     end
   end
 end
