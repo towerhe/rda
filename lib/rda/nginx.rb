5bdd2c093ecfa5a965f640681ef6dbc572dc6b6b
Tower He <towerhe@gmail.com>
2012-11-11 20:57:08 +0800

    Removed methods of discarding settings of nginx

--- lib/rda/nginx.rb
+++ lib/rda/nginx.rb
@@ -1,40 +1,14 @@
 module Rda
   class Nginx < Thor
     include Thor::Actions
-
-    def self.source_root
-      File.dirname(__FILE__)
-    end
+    include Helper
 
     desc "setup", "Set up your rails application"
     def setup(options = {})
       return unless installed?
 
-      @hostname, @environment = options["hostname"], options["environment"]
-
       mkdir_for_sites
-      set_passenger_user_and_group
       include_sites_enabled
-
-      template("templates/nginx", "#{conf_dir}/sites-available/#{hostname}")
-      link_file("#{conf_dir}/sites-available/#{hostname}", "#{conf_dir}/sites-enabled/#{hostname}")
-
-      unless configured?('/etc/hosts', "127.0.0.1  #{hostname}")
-        append_file "/etc/hosts", "127.0.0.1  #{hostname}"
-      end
-    end
-
-    desc "discard", "Remove the settings of your rails application from nginx"
-    def discard(options = {})
-      return unless installed?
-
-      @hostname = options["hostname"]
-
-      %W(enabled available).each do |n|
-        remove_file "#{conf_dir}/sites-#{n}/#{hostname}"
-      end
-
-      gsub_file("/etc/hosts", "127.0.0.1  #{hostname}", '')
     end
 
     private
@@ -54,32 +28,10 @@ module Rda
       $stderr.puts "ERROR: Missing `nginx.conf` in `#{conf_dir}`."
     end
 
-    def hostname
-      @hostname || "#{Rda::Rails.app_name}.local"
-    end
-
     def mkdir_for_sites
       %W(available enabled).each do |n|
         dir = conf_dir + "/sites-#{n}"
         empty_directory(dir) unless File.directory?(dir)
-      end
-    end
-
-    def set_passenger_user_and_group
-      conf = conf_dir + '/nginx.conf'
-
-      unless configured?(conf, 'passenger_default_user')
-        gsub_file conf, /http \{/, <<-PASSENGER
-http {
-    passenger_default_user root;
-        PASSENGER
-      end
-
-      unless configured?(conf, 'passenger_default_group')
-        gsub_file conf, /http \{/, <<-PASSENGER
-http {
-    passenger_default_group root;
-        PASSENGER
       end
     end
 
@@ -90,20 +42,6 @@ http {
     include #{conf_dir}/sites-enabled/*;
         INCLUDE_SITES_ENABLED
       end
-    end
-
-    def configured?(fname, conf)
-      File.open(fname) do |f|
-        f.readlines.each do |l|
-          if l.strip.start_with?(conf)
-            $stderr.puts "INFO: #{conf} has already been set!"
-
-            return true
-          end
-        end
-      end
-
-      false
     end
   end
 end
--- spec/lib/rda/nginx_spec.rb
+++ spec/lib/rda/nginx_spec.rb
@@ -5,9 +5,6 @@ describe Rda::Nginx do
 
   let(:conf_dir) { File.join(File.dirname(__FILE__), '../../tmp') }
 
-  before do
-  end
-
   describe '#setup' do
     context 'when the config file of nginx is not found' do
       before do
@@ -21,29 +18,6 @@ describe Rda::Nginx do
 ERROR: Missing `nginx.conf` in `/opt/nginx/conf`.
         PROMPT
       end
-    end
-  end
-
-  describe '#discard' do
-    let(:conf_dir) { File.dirname(__FILE__) + "/../../tmp/nginx" }
-
-    before do
-      FileUtils.mkdir_p conf_dir unless File.directory?(conf_dir)
-      FileUtils.copy_file(File.dirname(__FILE__) + "/../../fixtures/nginx.conf", conf_dir + '/nginx.conf')
-      Rda.stub_chain(:config, :nginx, :conf_dir).any_number_of_times.and_return(conf_dir)
-    end
-
-    after do
-      FileUtils.rm_r conf_dir if File.directory?(conf_dir)
-    end
-
-    it 'discards the settings' do
-      subject.should_receive(:gsub_file).with("/etc/hosts", "127.0.0.1  dummy.local", '')
-      %W(enabled available).each do |n|
-        subject.should_receive(:remove_file).with("#{conf_dir}/sites-#{n}/dummy.local")
-      end
-
-      subject.discard
     end
   end
 end
