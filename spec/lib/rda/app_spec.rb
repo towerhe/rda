248d4e532eb08f84f6dc8058bbe27612c72229ff
Tower He <towerhe@gmail.com>
2012-11-11 21:11:44 +0800

    Mock root of app

--- spec/lib/rda/app_spec.rb
+++ spec/lib/rda/app_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe Rda::App do
   subject { Rda::App.new }
 
-  before(:all) { Dir.chdir(File.join(File.dirname(__FILE__), '../../dummy')) }
+  let(:app_dir) { File.join(File.dirname(__FILE__), '../../dummy') }
 
   describe '#restart' do
+    before { Rda::Rails.should_receive(:root).any_number_of_times.and_return(app_dir) }
+
     it 'touches restart.txt' do
       FileUtils.should_receive(:touch).with(Rda::Rails.root.to_s + "/tmp/restart.txt")
 
