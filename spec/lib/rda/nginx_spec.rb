e89fefa0b47719587388aeb0759e28c17733153a
Tower He <towerhe@gmail.com>
2012-11-11 21:10:56 +0800

    Mock the name and path of rails app

--- spec/lib/rda/nginx_spec.rb
+++ spec/lib/rda/nginx_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe Rda::Nginx do
   subject { Rda::Nginx.new }
 
-  let(:conf_dir) { File.join(File.dirname(__FILE__), '../../tmp') }
+  let(:app_dir) { File.join(File.dirname(__FILE__), '../../dummy') }
 
   describe '#setup' do
     context 'when the config file of nginx is not found' do
       before do
+        Rda::Rails.should_receive(:app_name).any_number_of_times.and_return('dummy')
+        Rda::Rails.should_receive(:root).any_number_of_times.and_return(app_dir)
+
         File.should_receive(:exists?).with('/opt/nginx/conf/nginx.conf').and_return(false)
       end
 
